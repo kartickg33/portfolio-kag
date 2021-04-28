28a35476a8020885c284132def0e43f3b2ef74b2
kartickg33 <68003347+kartickg33@users.noreply.github.com>
2021-04-28 14:12:20 +0530

    added about and education

--- index.js
+++ index.js
@@ -5,13 +5,22 @@ var home = document.getElementById("home");
     });
     
 typewriter.typeString("Hi! I'm Kartick Gulati")
-    .pauseFor(2500)
+    .pauseFor(2000)
     .deleteChars(14)
     .typeString('a <strong>Full Stack Web Developer</strong>')
-    .pauseFor(2500)
+    .pauseFor(2000)
     .deleteChars(24)
     .typeString('<strong>Competitive Coder</strong>')
-    .pauseFor(2500)
-    .start();
+    .pauseFor(2000)
+    .deleteChars(21)
+    .typeString(' an Engineer')
+    .pauseFor(2000)
+    .deleteChars(11)
+    .typeString("a Tech Aficionado")
+    .pauseFor(2200)
+    .start()
+
+
+
 
 
